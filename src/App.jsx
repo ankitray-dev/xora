47a3f50e3073dfa14ef5855f6b5770a76b9aef83
AnkitRay-Dev <164015889+ankitray-dev@users.noreply.github.com>
2024-09-23 20:13:44 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Header from "./sections/Header"
+import Hero from "./sections/Hero"
+
 const App = () => {
   return (
     <main className="overflow-hidden">
-
+      <Header/>
+      <Hero/>
     </main>
   )
 }
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,37 @@
+import { Element, Link as LinkScroll } from "react-scroll";
+import Button from "../components/Button.jsx";
+
+const Hero = () => {
+  return (
+    <section className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32">
+      <Element name="hero">
+        <div className="container">
+          <div className="relative z-2 max-w-512 max-lg:max-w-388">
+            <div className="caption small-2 uppercase text-p3">
+                Effortless Video Editing
+            </div>
+            <h1 className="mb-6 h1 text-p4 uppercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
+                Incredibly intuitive
+            </h1>
+            <p className="max-w-440 mb-14 body-1 max-md:mb-10">
+                Meet XORA, the AI Video Editor crafted for simplicity, speed, and impressive power.
+            </p>
+            <LinkScroll to="features" offset={-100} spy smooth>
+              <Button icon="/images/zap.svg">Try it now</Button>
+            </LinkScroll>
+          </div>
+
+          <div className="absolute -top-32 left-[calc(50%-340px)] w-[1230px] pointer-events-none hero-img_res">
+            <img
+              src="/images/hero.png"
+              className="size-1230 max-lg:h-auto"
+              alt="hero"
+            />
+          </div>
+        </div>
+      </Element>
+    </section>
+  );
+};
+
+export default Hero;
